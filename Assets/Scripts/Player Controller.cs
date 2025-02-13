e62b7368dd6a2abc5c23d9ce5001fddca6e99816
Alef-Murick <alefcrow@gmail.com>
2025-02-13 06:46:57 -0300

    Player running and jumping animation

--- Assets/Scripts/Player Controller.cs
+++ Assets/Scripts/Player Controller.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody rb;
+    Animator anim;
+
     [SerializeField] float speed;
+    [SerializeField] float jumpForce;
+
 
 
     // Start is called before the first frame update
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
     void Start()
     {
@@ -21,7 +26,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("jump");
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 }
 
